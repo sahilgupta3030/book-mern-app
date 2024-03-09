38e062c994867c009279eeaf8424d0e110919559
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-03-09 22:03:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,16 +11,16 @@ app.use(express.json());
 
 // // middleware for handling CORS policy
 // // option 1: allow all origins with deafault of cors(*)
-app.use(cors());
+// app.use(cors());
 
 // // option 2: allow custom origins
-// app.use(
-//   cors({
-//     origin:'http://localhost:3000/',
-//     methods:['GET','POST','PUT','DELETE'],
-//     allowedHeaders:['Content-Type'],
-//   })
-// );
+app.use(
+  cors({
+    origin:'https://book-mern-sahilgupta.vercel.app/',
+    methods:['GET','POST','PUT','DELETE'],
+    allowedHeaders:['Content-Type'],
+  })
+);
 
 app.get("/", (request, response) => {
   console.log(request);
